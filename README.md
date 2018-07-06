3c55260233b63b733e28437283d5425e273d7b05
Deji Joseph <wrecodde@gmail.com>
2018-07-06 10:26:54 +0100

    okay, first step up

---
+++ README.md
@@ -0,0 +1,3 @@
+We fill this out later.
+
+let's get down to coding, shall we.
---
+++ lot.py
@@ -0,0 +1,57 @@
+# this module contains functions pertaining to the
+# parking lot.
+
+class NotEmpty(Exception):
+    pass
+
+class Car:
+    def __init__(self, plate_no, color):
+        self.plate_no = str(plate_no)
+        self.color = str(color)
+        self.parked = False
+        self.lot_no = None
+        self.ticket = None
+
+class Ticket:
+    def __init__(self, car, lot_no):
+        self.lot_no = None
+
+class Lot:
+    def __init__(self):
+        self.max_space = 6
+        self.spots = {}
+        self.populate()
+
+    def populate(self):
+        for lot in range(1, self.max_space + 1):
+            self.spots[lot] = ""
+        return
+
+    def is_empty(self):
+        # return a list of empty lots in ascending order
+        empty = []
+        for lot_no in self.spots:
+            if self.spots[lot_no] == "":
+                empty.append(lot_no)
+        return empty
+
+    def assign_ticket(self, lot_no, car):
+        # assign a ticket to a car for an open space
+        if len(self.spots) > self.max_space:
+            return # return some sort of error
+        self.spots[lot_no]= [car.plate_no, car.color]
+        ticket = Ticket(car, lot_no)
+
+def entry(plate_no, color):
+    """Process the entry of a car"""
+    # assign an open space if there is, and give out a ticket
+    if not Lot.is_empty():
+        raise NotEmpty("This Parking Lot is fully booked.")
+
+    car = Car(plate_no, color)
+
+    empty_lots = Lot.is_empty()
+    free_lot = empty_lots[0]
+    Lot.assign_ticket(free_lot, car)
+
+Lot = Lot()
