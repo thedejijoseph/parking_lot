c175ca0a64ff34faf20e9ed3c4be6e9800a6602a
Deji Joseph <wrecodde@gmail.com>
2018-07-06 13:02:51 +0100

    added tests for lot.build_lot

--- lot_test.py
+++ lot_test.py
@@ -11,5 +11,17 @@ class TestCar(unittest.TestCase):
         car = lot.Car("axs0989", "matte black")
         self.assertEqual("Matte Black", car.color)
 
+class TestTicket(unittest.TestCase):
+    def test_nothing(self):
+        pass
+
+class TestLot(unittest.TestCase):
+    """This should contain a lot of test methods."""
+    # do you see what i did there :) ;)
+    def test_build_lot(self):
+        test_lot = lot.Lot(12)
+        test_lot.build_lot()
+        self.assertEqual(12, len(test_lot.slots))
+
 if __name__ == "__main__":
     unittest.main()
