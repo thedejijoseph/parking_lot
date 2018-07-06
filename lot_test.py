44b639ffafb3bea28577817113641f720b9718ff
Deji Joseph <wrecodde@gmail.com>
2018-07-06 11:01:16 +0100

    wrote another test for car.color

--- lot_test.py
+++ lot_test.py
@@ -6,6 +6,10 @@ class TestCar(unittest.TestCase):
     def test_plate_no_transform(self):
         car = lot.Car("axs0989", "matte black")
         self.assertEqual("AXS0989", car.plate_no)
+    
+    def test_color_transform(self):
+        car = lot.Car("axs0989", "matte black")
+        self.assertEqual("Matte Black", car.color)
 
 if __name__ == "__main__":
     unittest.main()
